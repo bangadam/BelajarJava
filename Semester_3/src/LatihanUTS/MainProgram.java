7213850bf21b240a428cf5e7fe3012667657263a
bangadam <bangadam.dev@gmail.com>
2019-10-13 21:50:50 +0700

    update UTS

---
+++ Semester_3/src/LatihanUTS/MainProgram.java
@@ -0,0 +1,8 @@
+package LatihanUTS;
+
+public class MainProgram extends Kampus {
+    public static void main(String[] args) {
+        MainProgram ob = new MainProgram();
+        ob.prosesInsert();
+    }
+}
